0fd822b45d393b71f3469f23fc5ef60395525633
Animesh Baranawal <animeshbaranawal@gmail.com>
2016-08-10 23:13:21 +0530

    Changed CLAHE

--- assignments/assignment1_GrayTrans/2/code/adaptiveHistogramcutoff.m
+++ assignments/assignment1_GrayTrans/2/code/adaptiveHistogramcutoff.m
@@ -1,18 +1,14 @@
-function [ x ] = adaptiveHistogramcutoff( A )
+function [ x ] = adaptiveHistogramcutoff( A , cutOff)
 %UNTITLED3 Summary of this function goes here
 %   Detailed explanation goes here
-%A = imread('../data/barbara.png');
-%h = histogram(A,256);
-temp = zeros(256);
-uniqueA = unique(A);
-temp(uniqueA'+1) = histc(A(:),uniqueA);
-fractions = temp/(size(A,1)*size(A,2));
-abovecutoff = fractions > 0.005;
-total = sum(fractions(abovecutoff) - 0.005);
-fractions(abovecutoff) = 0.005;
+
+fractions = histcounts(A,0:1:256)/(size(A,1)*size(A,2));
+abovecutoff = fractions > cutOff;
+total = sum(fractions(abovecutoff) - cutOff);
+fractions(abovecutoff) = cutOff;
 fractions = fractions + (total/256);
 intensity = A((size(A,1))/2,(size(A,2))/2);
-
 x = sum(fractions(1:intensity+1))*255;
+
 end
 
--- assignments/assignment1_GrayTrans/2/code/myCLAHE.m
+++ assignments/assignment1_GrayTrans/2/code/myCLAHE.m
@@ -1,14 +1,16 @@
-function [output] = myCLAHE(input , windowSize)
+function [output] = myCLAHE(input , windowSize, cutOff)
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
 
     columnCount = size(input,2);
     rowCount = size(input,1);
     output = zeros(rowCount,columnCount,size(input,3));
+    f = @(A) adaptiveHistogramcutoff(A, cutOff);
     
     for i = 1:size(output,3)
-        output(:,:,i) = nlfilter(input(:,:,i),'indexed',[windowSize windowSize],@adaptiveHistogramcutoff);
+        output(:,:,i) = nlfilter(input(:,:,i),'indexed',[windowSize windowSize],f);
     end
+    
     if size(input,3) == 3
         output = uint8(output);
     end
